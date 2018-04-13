eccf2d7e5c24328016b37cf383e43a3af436b883
tafler <taffik@yandex.ru>
2018-04-13 12:15:18 +0300

    first commit

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,46 @@
-$(document).ready(function(){
-    var nav = $("#main-nav");
-    $("#mainburger").click(function(){
-        console.log("jquery good");
-        nav.
-    })
+
+let more = document.querySelectorAll('.services__block__preload a');
+Array.prototype.forEach.call(more, function(el, i){
+    console.log(el[i] + " + " + i);
+    el.addEventListener('click', function(event) {
+        console.log(el + " + " + i);
+        event.preventDefault();
+        let parent = el.closest('.services__block');
+        parent.querySelector('.card').style.display = 'none';
+        parent.querySelector('.services__block__info').style.display = 'block';
+    }, false);
 });
+
+
+let less = document.querySelectorAll('.services__block__info a');
+Array.prototype.forEach.call(less, function(el, i){
+    console.log(el[i] + " + " + i);
+    el.addEventListener('click', function(event) {
+        console.log(el + " + " + i);
+        event.preventDefault();
+        let parent = el.closest('.services__block');
+        parent.querySelector('.card').style.display = 'block';
+        parent.querySelector('.services__block__info').style.display = 'none';
+    }, false);
+});
+
+
+  
+
+$(document).ready(function(){
+    $('.about__slider__image').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 1,
+      
+        adaptiveHeight: true
+    });
+    $('.comments__slider').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1
+      
+    });
+  });
+
+
